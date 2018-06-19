aa228d88327e4c0e7924bd784837e822d55f0a4b
Nick Costigan <nick.costigan@gmail.com>
2018-06-19 14:03:29 -0400

    jenkins test

---
+++ jenkins-test/src/main/java/com/revature/servlets/TestServlet.java
@@ -0,0 +1,26 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+    public TestServlet() {
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().print("Testing out my pipeline!!");
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
